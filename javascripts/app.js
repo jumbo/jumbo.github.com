49ba436df5d9c02ab2347155350bdfd636c65920
jumbo <jumbo.yin@gmail.com>
2012-11-24 00:59:42 +0800

    add projects

--- javascripts/app.js
+++ javascripts/app.js
@@ -6,9 +6,5 @@ angular.module('resume',[]).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
       when('/all', {templateUrl: 'template-all.html',  controller: ResumeListCtrl}).
-      when('/education', {templateUrl: 'template-education.html',  controller: ResumeListCtrl}).
-      when('/experience', {templateUrl: 'template-experience.html',  controller: ResumeListCtrl}).
-      when('/skills', {templateUrl: 'template-skills.html',  controller: ResumeListCtrl}).
-      when('/projects', {templateUrl: 'template-projects.html',  controller: ResumeListCtrl}).
       otherwise({redirectTo: '/all'});
 }]);
